198317b001a5a9852fdddb8ea84b405d241efcaf
AbnerLimaa <abner.lima27@outlook.com>
2026-08-20 13:37:55 -0300

    feat: customize theme, colors, and update navbar layout

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: "Caja",
+  title: "Cajá",
   description: "The modern programming language",
+  head: [
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    ['link', { href: 'https://fonts.googleapis.com/css2?family=Modak&display=swap', rel: 'stylesheet' }]
+  ],
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
@@ -33,8 +38,22 @@ export default defineConfig({
       }
     ],
 
+    search: {
+      provider: 'local',
+      options: {
+        placeholder: 'Search documentation...'
+      }
+    },
+
     socialLinks: [
       { icon: 'github', link: 'https://github.com/caja-tech/cajadoc-webpage' }
-    ]
+    ],
+
+    markdown: {
+      theme: {
+        light: 'github-light',
+        dark: 'github-dark'
+      }
+    },
   }
 })
